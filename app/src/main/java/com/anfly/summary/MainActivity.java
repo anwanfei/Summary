be4fc9ea030904207d220ade3561d373be25b6a4
anwanfei <546513254@qq.com>
2019-11-18 23:42:00 +0800

    1.navigation与toolbar结合，首页随着侧滑菜单移动 2.设置toolbar主题

--- app/src/main/java/com/anfly/summary/MainActivity.java
+++ app/src/main/java/com/anfly/summary/MainActivity.java
@@ -1,14 +1,87 @@
 package com.anfly.summary;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.anfly.summary.base.BaseActivity;
+import com.google.android.material.navigation.NavigationView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.fl_container)
+    FrameLayout flContainer;
+    @BindView(R.id.rb_home)
+    RadioButton rbHome;
+    @BindView(R.id.rg)
+    RadioGroup rg;
+    @BindView(R.id.nv)
+    NavigationView nv;
+    @BindView(R.id.dl)
+    DrawerLayout dl;
+    @BindView(R.id.ll_main)
+    LinearLayout llMain;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+        super.initView();
+        toolbar.setTitle(getResources().getString(R.string.home));
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dl, toolbar, R.string.open, R.string.close);
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    protected void initListener() {
+        super.initListener();
+        dl.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
+                int right = drawerView.getRight();
+                llMain.setX(right);
+            }
+
+            @Override
+            public void onDrawerOpened(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(@NonNull View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // TODO: add setContentView(...) invocation
+        ButterKnife.bind(this);
     }
 }
