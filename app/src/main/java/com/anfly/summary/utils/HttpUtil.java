9d9253323aadf68c270a3f71fba023fdb140a077
anwanfei <546513254@qq.com>
2019-11-18 22:44:56 +0800

    网络工具封装

--- app/src/main/java/com/anfly/summary/utils/HttpUtil.java
+++ app/src/main/java/com/anfly/summary/utils/HttpUtil.java
@@ -1,51 +1,110 @@
 package com.anfly.summary.utils;
 
-import android.content.Context;
-import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
+import java.io.File;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
-/**
- * @author Anfly
- * @date 2019/3/5.
- * description：网络判断工具类：包括网络是否可用；获取当前网络类型
- */
+import io.reactivex.android.BuildConfig;
+import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class HttpUtil {
+    private static HttpUtil httpUtil;
+    private final Retrofit.Builder builder;
 
-    /**
-     * 判断网路是否可用
-     * @param context
-     * @return
-     */
-    public static boolean isNetWorkAvailable(Context context){
-        ConnectivityManager manager  = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        //获取所有可用的网络连接信息
-        NetworkInfo[] infos = manager.getAllNetworkInfo();
-        if(null != infos){
-            for(NetworkInfo info : infos){
-                if(info.getState() == NetworkInfo.State.CONNECTED){
-                    return true;
+    public static HttpUtil getHttpUtil() {
+        if (httpUtil == null) {
+            synchronized (HttpUtil.class) {
+                if (httpUtil == null) {
+                    httpUtil = new HttpUtil();
                 }
             }
         }
-        return false;
+        return httpUtil;
+    }
+
+    public HttpUtil() {
+        builder = new Retrofit.Builder()
+                .client(getOkHttpClient())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create());
+    }
+
+    private OkHttpClient getOkHttpClient() {
+
+        //设置本地缓存文件
+        File file = new File(Constants.PATH_CACHE);
+        //设置缓存文件大小
+        Cache cache = new Cache(file, 1024 * 1024 * 50);
+        //设置日志拦截器
+        HttpLoggingInterceptor httpLoggingInterceptor = new HttpLoggingInterceptor();
+        if (BuildConfig.DEBUG) {
+            httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.HEADERS);
+        } else {
+            httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.NONE);
+        }
+
+
+        return new OkHttpClient.Builder()
+                .cache(cache)
+                .addNetworkInterceptor(httpLoggingInterceptor)
+                .addInterceptor(new CacheInterceptor())
+                .addInterceptor(new CacheInterceptor())
+                .connectTimeout(10, TimeUnit.SECONDS)
+                .readTimeout(10, TimeUnit.SECONDS)
+                .writeTimeout(10, TimeUnit.SECONDS)
+                .retryOnConnectionFailure(true)
+                .build();
+    }
+
+    public <T> T getServer(String baseUrl, Class<T> mClass) {
+        return builder.baseUrl(baseUrl).build().create(mClass);
     }
 
     /**
-     * 返回网络类型
-     * @return
+     * 缓存拦截器
      */
-    public static NetWorkState getNetworkState(Context context){
-        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if(manager.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_WIFI){
-            return NetWorkState.WIFI;
-        }else if(manager.getActiveNetworkInfo().getType() == ConnectivityManager.TYPE_MOBILE){
-            return NetWorkState.MOBILE;
-        }else {
-            return NetWorkState.NONE;
+    private class CacheInterceptor implements Interceptor {
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+            Request request = chain.request();
+            //这里就是说判读我们的网络条件，要是有网络的话我么就直接获取网络上
+            // 面的数据，要是没有网络的话我么就去缓存里面取数据
+            if (!SystemUtil.isNetworkConnected()) {
+                request = request
+                        .newBuilder()
+                        .cacheControl(CacheControl.FORCE_CACHE)
+                        .build();
+            }
+            Response originalResponse = chain.proceed(request);
+            if (SystemUtil.isNetworkConnected()) {
+                int maxAge = 0;
+                // 有网络时, 不缓存, 最大保存时长为0
+                return originalResponse.newBuilder()
+                        .removeHeader("Pragma")
+                        .header("Cache-Control", "public ,max-age=" + maxAge)
+                        .build();
+            } else {
+                //s秒
+                int maxStale = 60 * 60 * 24;
+                return originalResponse.newBuilder()
+                        .removeHeader("Pragma")
+                        //这里的设置的是我们的没有网络的缓存时间，
+                        // 想设置多少就是多少。
+                        .header("Cache-Control", "public, only-if-cached," +
+                                " max-stale=" + maxStale)
+                        .build();
+            }
+
         }
     }
 
-    public enum NetWorkState {
-        WIFI,MOBILE,NONE
-    }
 }
